2d18cee9dfcbd56409a2d31c2b4d895ebb83a406
clare <chblackburne@gmail.com>
2019-01-10 14:19:11 +0000

    config incl webpacker

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'invoices/index'
-  get 'invoices/show'
+  root to: 'invoices#index'
+
+  resources :invoices, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
